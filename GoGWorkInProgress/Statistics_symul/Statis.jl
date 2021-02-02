b22772b4196bf23fa9122d5bf3847de2daedd4a4
Paul-Adrian Gogîță <gogitapaul@yahoo.ro>
2021-02-02 14:26:59 +0200

    Final version for now

--- GoGWorkInProgress/Statistics_symul/Statis.jl
+++ GoGWorkInProgress/Statistics_symul/Statis.jl
@@ -3,7 +3,7 @@ using Distributions
 using Random
 using StatsPlots
 
-Numar_aruncari = 100;
+Numar_aruncari = 1000;
 
 # Generam aruncarile cap -> 1, pajura -> 0
 N_esantion = 0:Numar_aruncari;
@@ -11,7 +11,7 @@ p_true = 0.5;
 Random.seed!(12);
 data = rand(Bernoulli(p_true), last(N_esantion));
 
-# Introducem bias
+# Introducem bias - metoda simpla/barbara de a introduce un bias care mai apoi sa se corecteze (se poate dezvolta)
 #data = fill(1, 50)
 #data = vcat(data, rand(Bernoulli(p_true), 50))
 
@@ -45,3 +45,6 @@ for (i, N) in enumerate(N_esantion)
 end;
 
 gif(anim, "CoinTossAveraging.gif", fps = 10)
+
+# Pentru viitor: comparatie pentru acelasi set de date -> inferenta Bayesiana vs o inferenta descriptiva simpla cu distributie Gauss,
+# (μ = sum(data) & σ cu formula consacrata la fiecare iteratie) pentru comparare -> caz cinstit vs caz cu bias (outlieri)
