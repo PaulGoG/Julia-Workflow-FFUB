8a2a40555fbd836f656ec6428d0dc9ebe9101013
Paul-Adrian Gogîță <gogitapaul@yahoo.ro>
2021-04-16 01:26:13 +0300

    Work in progress

--- GoGWorkInProgress/Licenta/Calcul_dilutie.jl
+++ GoGWorkInProgress/Licenta/Calcul_dilutie.jl
@@ -1,19 +1,19 @@
 #=
 Formule diferite pentru calculul dilutiei atmosferice χ/Q
 In cazul nostru χ/Q este o matrice NxNxN sau NXN (z=0, la sol)
+!Simulam pana de poluant pentru care x>0 pe directia vantului!
 =#
 
-# Dilutia instantanee sau scurta durata (3 minute -> o ora) -> vector 3D
-function dilutie_instantanee(x, y, z, Pasquill)
-    # Calculul chestiilor din formula cu functii
-    # Se calculeaza pentru o anumita clasa Pasquill
-    H = 
-    Σ_z = 
-    Σ_y = 
-    u = 
-
-    χ_Q = 1/(2*π*Σ_y*Σ_z*u) * exp(-y^2/(2*Σ_y^2)) * (exp(-(z-H)^2/(2*Σ_z^2)) + exp(-(z+H)^2/(2*Σ_z^2))) * f_inversie_termica(Σ_z, H, h_i)
-    return χ_Q
+#=
+Dilutia instantanee sau scurta durata (3 minute -> o ora) -> vector 3D
+A nu se apela in punctul de emisie sau impotriva vantului (x<=0)!
+=#
+function dilutie_instantanee(x, y, z, Pasquill, Suprafata, Tip_Suprafata)
+    H = H_final(x, Pasquill, Suprafata)
+    Σz = Σ_z(x, Pasquill, Suprafata, Tip_Suprafata)
+    Σy = Σ_y(x, Pasquill, Suprafata)
+    u = u_z(H, Pasquill, Suprafata)
+    return 1/(2*π*Σy*Σz*u) * exp(-y^2/(2*Σy^2)) * (exp(-(z-H)^2/(2*Σz^2)) + exp(-(z+H)^2/(2*Σz^2)))
 end
 
 #=
@@ -21,34 +21,32 @@ Dilutie durata _prelungita (o ora -> 24 ore)
 Depinde de x variabil, y ∈ sector de cerc, z=0 => valori care depind doar de variatia lui x si
 Apartententa la un sector unghiular de cerc
 Mai exact dependenta explicita va fi doar de x, cea de y fiind folosita pt calculul sectorului unghiular prin arctan
-Astfel, valorile o sa fie linii // cu OY marginite de marginile "evantaiului" sectoarelor de cerc
+Astfel, valorile o sa fie linii // cu OY marginite de marginile "evantaiului" sectorului de cerc
 =#
-function dilutie_durata_prelungita(x, y)
-
-    # N_sector = floor(atan(abs(y/x)/θ_L)) + 1
-    # Se calculeaza pentru o anumita clasa Pasquill
-    H = 
-    Σ_z = 
-    u = 
-    χ_Q = (2/π)^0.5 * 1/(Σ_z*u*x*θ_L) * exp(-H^2/(2*Σ_z^2)) * f_inversie_termica(Σ_z, H, h_i)
-    return χ_Q
+function dilutie_durata_prelungita(x, y, Pasquill, Suprafata, Tip_Suprafata)
+    if abs(y/x) <= Sector_Cerc 
+    H = H_final(x, Pasquill, Suprafata)
+    Σz = Σ_z(x, Pasquill, Suprafata, Tip_Suprafata)
+    u = u_z(H, Pasquill, Suprafata)
+    return (2/π)^0.5 * 1/(Σz*u*x*θ_L) * exp(-H^2/(2*Σz^2))
+    end
+    return 0.0
 end
 
 # Dilutie lunga_durata (> 24 ore)
 # Aici nu mai exista dependenta explicita nici macar de x, totul se considera pe termen foarte lung
 # => Sectoare de cerc in care dilutia e cuasi-omogena, depinde de coordonate doar implicit
-function dilutie_lunga_durata(x, y)
-    # Frecventele se citesc din DataFrame-ul corespunzator
-    k = floor(atan(abs(y/x)/θ_L)) + 1
-    F_k = first(Freq.F_k[(Freq.Zona_k .== k)])
-    F_ki = Freq.F_ki[(Freq.Zona_k .== k)]
-    H = 
-    Σ_z = 
-    u = 
-    Parametru = exp(-H^2/(2*Σ_z^2)) * f_inversie_termica(Σ_z, H, h_i)/(Σ_z*u) 
-    # Avand in vedere ca toate depind de Pasquill, trebuie ca Parametru sa fie si el vector
-    # La fel ca F_ki => Parametru il construim intr-un for alaturi de celalalte calcule pt a trece
-    # Prin toate clasele Pasquill cu calculele 
-    χ_Q = (2/π)^0.5 * F_k/(x*θ_L) * sum(F_ki .* Parametru)
-    return χ_Q
+function dilutie_lunga_durata(x, y, Suprafata, Tip_Suprafata)
+    k = Apartenenta_Sector_Cerc(x, y)
+    F_k = Freq.F_k[(Freq.Zona_k .== k)][1]
+    Suma = 0
+    for i in 1:6
+        Pasquill = IntegertoString(i)
+        H = H_final(x, Pasquill, Suprafata)
+        Σz = Σ_z(x, Pasquill, Suprafata, Tip_Suprafata)
+        u = u_z(H, Pasquill, Suprafata)
+        F_ki = Freq.F_ki[(Freq.Zona_k .== k) .& (Freq.Clasa_Pasquill .== Pasquill)][1]
+        Suma = Suma + F_ki * exp(-H^2/(2*Σz^2))/(Σz*u) 
+    end
+    return (2/π)^0.5 * F_k/(x*θ_L) * Suma
 end
---
+++ GoGWorkInProgress/Licenta/SectoareCerc.jl
@@ -0,0 +1,27 @@
+using Plots; gr()
+using Trapz
+using DataFrames
+using CSV
+
+include("Constante.jl")
+include("Helpers.jl")
+
+θ = collect(0:0.1:2*π)
+x = 100*cos.(θ)
+y = 100*sin.(θ)
+xprim = collect(-100:1:100)
+plot(x,y, legend = false);
+hline!([0]);
+vline!([0]);
+for l in 1:(n/2)
+    if l%4 != 0
+    yprim = [tan(l*θ_L)*xprim[o] for o in 1:length(xprim)]
+    yprim = yprim[((yprim[:].^2 + xprim[:].^2) .< 10000)]
+    xplot = collect(-length(yprim)/2:1:length(yprim)/2 -1)
+    plot!(xplot,yprim)
+    end
+end
+a = x[rand(1:1:length(x))]
+b = y[rand(1:1:length(y))]
+scatter!([a],[b])
+Apartenenta_Sector_Cerc(a,b)
