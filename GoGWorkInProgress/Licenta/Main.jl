5cfd0773ddd711313f38780a68b4e835dc96f4f5
Paul-Adrian Gogîță <gogitapaul@yahoo.ro>
2021-04-18 14:19:54 +0300

    Adaugat rutinele de facut grafice
    Mai trebuie aranjate ce/cum apare, culori, legenda, rotatie camera

--- GoGWorkInProgress/Licenta/Main.jl
+++ GoGWorkInProgress/Licenta/Main.jl
@@ -9,7 +9,7 @@ using Trapz
 using DataFrames
 using CSV
 
-t_R = 60*60*365*17 # Timpul de emisie in secunde
+t_R = 86400*365/2 # Timpul de emisie in secunde
 t_zile = t_R/86400
 t_spalare = t_R * (15/365) # Consideram ca avem 15 zile ploioase intr-un an
 
@@ -24,10 +24,10 @@ Pasquill = "D"
 Suprafata = "Padure_Oras"
 Tip_Suprafata = "Padure_Urban"
 Tip_Aversa = "Ploaie"
-Debit = 1.0
+Debit = 3.0
 dim_transversal = 10000
 dim_vertical = 0
-step = 1000
+step = 10
 
 x = collect(-(dim_transversal/2):step:(dim_transversal/2))
 y = collect(-(dim_transversal/2):step:(dim_transversal/2))
@@ -37,12 +37,48 @@ if t_R <= 3600
     χ_Q = χ_Q_Durata_Scurta(x, y, z, Pasquill, Suprafata, Tip_Suprafata, t_R)[:,:,1]
     χ = χ_Durata_Scurta(χ_Q, x, y, z, Pasquill, Suprafata, Tip_Suprafata, Tip_Aversa, Debit, t_spalare)[:,:,1]
     ω = ω_Scurt(χ_Q, x, y, Pasquill, Suprafata, Tip_Suprafata, Tip_Aversa, Debit, t_R, t_spalare)
+
+    Reprezinta_Suprafata(x, y, χ_Q, x[Int(dim_transversal/2 + 1)], "χ/Q (s/m^3)")
+    Reprezinta_Gradient(x, y, χ_Q, x[Int(dim_transversal/2 + 1)], "χ/Q (s/m^3)")
+    Reprezinta_Contur(x, y, χ_Q, x[Int(dim_transversal/2 + 1)], "χ/Q (s/m^3)")
+
+    Reprezinta_Suprafata(x, y, χ, x[Int(dim_transversal/2 + 1)], "χ (Bq s/m^3)")
+    Reprezinta_Gradient(x, y, χ, x[Int(dim_transversal/2 + 1)], "χ (Bq s/m^3)")
+    Reprezinta_Contur(x, y, χ, x[Int(dim_transversal/2 + 1)], "χ (Bq s/m^3)")
+
+    Reprezinta_Suprafata(x, y, ω, x[Int(dim_transversal/2 + 1)], "ω (Bq/m^2)")
+    Reprezinta_Gradient(x, y, ω, x[Int(dim_transversal/2 + 1)], "ω (Bq/m^2)")
+    Reprezinta_Contur(x, y, ω, x[Int(dim_transversal/2 + 1)], "ω (Bq/m^2)")
 elseif t_R <= 86400
     χ_Q = χ_Q_Durata_Prelungita(x, y, Pasquill, Suprafata, Tip_Suprafata)
     χ = χ_Durata_Prelungita(χ_Q, x, y, Pasquill, Suprafata, Tip_Suprafata, Tip_Aversa, Debit, t_spalare)
     ω = ω_Prelungit(χ_Q, x, y, Pasquill, Suprafata, Tip_Suprafata, Tip_Aversa, Debit, t_R, t_spalare)
+
+    Reprezinta_Suprafata(x, y, χ_Q, x[Int(dim_transversal/2 + 1)], "χ/Q (s/m^3)")
+    Reprezinta_Gradient(x, y, χ_Q, x[Int(dim_transversal/2 + 1)], "χ/Q (s/m^3)")
+    Reprezinta_Contur(x, y, χ_Q, x[Int(dim_transversal/2 + 1)], "χ/Q (s/m^3)")
+
+    Reprezinta_Suprafata(x, y, χ, x[Int(dim_transversal/2 + 1)], "χ (Bq s/m^3)")
+    Reprezinta_Gradient(x, y, χ, x[Int(dim_transversal/2 + 1)], "χ (Bq s/m^3)")
+    Reprezinta_Contur(x, y, χ, x[Int(dim_transversal/2 + 1)], "χ (Bq s/m^3)")
+
+    Reprezinta_Suprafata(x, y, ω, x[Int(dim_transversal/2 + 1)], "ω (Bq/m^2)")
+    Reprezinta_Gradient(x, y, ω, x[Int(dim_transversal/2 + 1)], "ω (Bq/m^2)")
+    Reprezinta_Contur(x, y, ω, x[Int(dim_transversal/2 + 1)], "ω (Bq/m^2)")
 else
     χ_Q = χ_Q_Durata_Lunga(x, y, Suprafata, Tip_Suprafata)
     χ = χ_Durata_Lunga(χ_Q, x, y, Suprafata, Tip_Suprafata, Tip_Aversa, Debit, t_spalare)
     ω = ω_Lung(χ_Q, x, y, Suprafata, Tip_Suprafata, Tip_Aversa, Debit, t_spalare)
+
+    Reprezinta_Suprafata(x, y, χ_Q, x[1], "χ/Q (s/m^3)")
+    Reprezinta_Gradient(x, y, χ_Q, x[1], "χ/Q (s/m^3)")
+    Reprezinta_Contur(x, y, χ_Q, x[1], "χ/Q (s/m^3)")
+
+    Reprezinta_Suprafata(x, y, χ, x[1], "χ (Bq s/m^3)")
+    Reprezinta_Gradient(x, y, χ, x[1], "χ (Bq s/m^3)")
+    Reprezinta_Contur(x, y, χ, x[1], "χ (Bq s/m^3)")
+
+    Reprezinta_Suprafata(x, y, ω, x[1], "ω (Bq/m^2)")
+    Reprezinta_Gradient(x, y, ω, x[1], "ω (Bq/m^2)")
+    Reprezinta_Contur(x, y, ω, x[1], "ω (Bq/m^2)")
 end
