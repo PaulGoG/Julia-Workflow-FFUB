ed27aaa6959f6b49577973d53f27bcde420f2a14
Paul-Adrian Gogîță <gogitapaul@yahoo.ro>
2022-02-22 01:19:16 +0200

    Update Radionuclizi_1.jl

--- GoGWorkInProgress/Radionuclizi_M_1/Radionuclizi_1.jl
+++ GoGWorkInProgress/Radionuclizi_M_1/Radionuclizi_1.jl
@@ -27,16 +27,21 @@ function Energie_medie_legatura(librarie)
     Dⁿ = df.D[(df.A .== 1) .& (df.Z .== 0)][1]
     σⁿ = df.σ[(df.A .== 1) .& (df.Z .== 0)][1]
 
+    # Parcurgem toti radionuclizii din baza de date dupa A si dupa Z
     for i in 2:maximum(df.A)
         Z_min = minimum(df.Z[df.A .== i])
         Z_max = maximum(df.Z[df.A .== i])
-        for j in Z_min:Z_max # Parcurgem toti radionuclizii din baza de date dupa A si dupa Z
+        for j in Z_min:Z_max 
+            # Selectare rubrici de interes pentru elementul (A,Z)
             D = df.D[(df.A .== i) .& (df.Z .== j)][1]
             σᴰ = df.σ[(df.A .== i) .& (df.Z .== j)][1]
 
+            # Calculul valorilor
             W = j*Dᵖ + (i-j)*Dⁿ - D
             σᵂ = sqrt(j^2 * σᵖ^2 + (i-j)^2 * σⁿ^2 + σᴰ^2)
-
+            
+            # Scrierea valorilor in structura de vectori de vectori
+            # Valorile energiilor sunt calculate in MeVi
             push!(radionuclid.B, W/i *1e-3)
             push!(radionuclid.σᴮ, σᵂ/i *1e-3)
             push!(radionuclid.A, i)
@@ -44,12 +49,13 @@ function Energie_medie_legatura(librarie)
         end
     end 
 
-    # Figuram doar valorile mai mari de 3 MeV pentru scalare buna a axelor
+    # Figuram doar valorile mai mari de 3 MeV pentru scalarea buna a axelor
     deleteat!(radionuclid.A, findall(x -> x <=3, radionuclid.B))
     deleteat!(radionuclid.σᴮ, findall(x -> x <=3, radionuclid.B))
     deleteat!(radionuclid.B, findall(x -> x <= 3, radionuclid.B))
 
     # Reprezentari grafice
+    # Scatter simplu
     plt1 = scatter(
     radionuclid.A, 
     radionuclid.B,  
@@ -59,7 +65,7 @@ function Energie_medie_legatura(librarie)
     ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
     framestyle = :box,
     legend = :false,
-    title = "Energia medie de legătură per nucleon",
+    title = "Energia medie de legătură per nucleon, $(librarie[begin:end-4])",
     minorgrid = :true,
     mc = :blue, 
     size = (1000, 1000)
@@ -67,6 +73,7 @@ function Energie_medie_legatura(librarie)
     display(plt1)
     savefig(plt1, "Grafice\\B_toti_radionuclizii_$(librarie[begin:end-4]).png")
 
+    # Scatter cu incertitudini
     plt1 = scatter(
     radionuclid.A, 
     radionuclid.B, 
@@ -77,7 +84,7 @@ function Energie_medie_legatura(librarie)
     ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
     framestyle = :box,
     legend = :false,
-    title = "Energia medie de legătură per nucleon",
+    title = "Energia medie de legătură per nucleon, $(librarie[begin:end-4])",
     minorgrid = :true,
     msc = :olivedrab,
     mc = :cyan, 
@@ -89,7 +96,11 @@ function Energie_medie_legatura(librarie)
     return radionuclid # Obiectul returnat de functie este un vector de vectori 
 end
 
+Energie_medie_legatura("AUDI95.csv")
+Energie_medie_legatura("AUDI2021.csv")
+
 radionuclid = Energie_medie_legatura("AUDI95.csv")
+librarie = "AUDI95.csv"
 
     plt1 = scatter(
     radionuclid.A, 
@@ -116,12 +127,46 @@ radionuclid = Energie_medie_legatura("AUDI95.csv")
     ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
     framestyle = :box,
     legend = :false,
-    title = "Energia medie de legătură per nucleon",
+    title = :false,
     minorgrid = :true,
     msc = :olivedrab,
     mc = :cyan, 
     size = (900, 900)
     )
-    annotate!(150, 6.5, L"\sigma_{B(A,Z)} = \frac{1}{A} \sqrt{Z^2 \sigma_{D_p}^2 + (A-Z)^2 \sigma_{D_n}^2 + \sigma_{D(A,Z)}^2}")
-    plt = plot(plt1, plt2, layout = (2, 1))
-    savefig(plt1, "Grafice\\B_toti_radionuclizii_errb_$(librarie[begin:end-4]).png")
+
+    plt3 = scatter(
+    radionuclid.A, 
+    radionuclid.B, 
+    yerr = radionuclid.σᴮ, 
+    marker = :xcross,
+    markersize = 3, 
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
+    framestyle = :box,
+    legend = :false,
+    title = :false,
+    minorgrid = :true,
+    msc = :olivedrab,
+    mc = :cyan, 
+    size = (900, 900)
+    )
+
+    plt4 = scatter(
+    radionuclid.A, 
+    radionuclid.B, 
+    yerr = radionuclid.σᴮ, 
+    marker = :xcross,
+    markersize = 3, 
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
+    framestyle = :box,
+    legend = :false,
+    title = :false,
+    minorgrid = :true,
+    msc = :olivedrab,
+    mc = :cyan, 
+    size = (900, 900)
+    )    
+
+    plt = plot(plt1, plt2, plt3, plt4, layout = (2, 2))
+    savefig(plt, "Grafice\\B_layout_4.png")
