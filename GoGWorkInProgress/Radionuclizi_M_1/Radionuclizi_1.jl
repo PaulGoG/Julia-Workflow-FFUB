f4180b6c0d6a8d3a329b0234d0d0b8da09558d87
Paul-Adrian Gogita <gogitapaul@yahoo.ro>
2022-02-22 14:00:50 +0200

    Update Radionuclizi_1.jl
    Final form for now

--- GoGWorkInProgress/Radionuclizi_M_1/Radionuclizi_1.jl
+++ GoGWorkInProgress/Radionuclizi_M_1/Radionuclizi_1.jl
@@ -51,16 +51,22 @@ function Energie_medie_legatura(librarie)
 
     # Figuram doar valorile mai mari de 3 MeV pentru scalarea buna a axelor
     deleteat!(radionuclid.A, findall(x -> x <=3, radionuclid.B))
+    deleteat!(radionuclid.Z, findall(x -> x <=3, radionuclid.B))
     deleteat!(radionuclid.σᴮ, findall(x -> x <=3, radionuclid.B))
     deleteat!(radionuclid.B, findall(x -> x <= 3, radionuclid.B))
 
-    # Reprezentari grafice
-    # Scatter simplu
-    plt1 = scatter(
+    return radionuclid # Obiectul returnat de functie este un vector de vectori de tipul structului
+end
+# Reprezentari grafice
+
+# Scatter simplu
+function Grafic_simplu(radionuclid, librarie)
+    plt = scatter(
     radionuclid.A, 
     radionuclid.B,  
     marker = :xcross,
     markersize = 3, 
+    markerstrokewidth = 0,
     xlabel = L"\mathrm{A}", 
     ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
     framestyle = :box,
@@ -70,103 +76,178 @@ function Energie_medie_legatura(librarie)
     mc = :blue, 
     size = (1000, 1000)
     )
-    display(plt1)
-    savefig(plt1, "Grafice\\B_toti_radionuclizii_$(librarie[begin:end-4]).png")
-
-    # Scatter cu incertitudini
+    display(plt)
+    savefig(plt, "Grafice\\B_toti_radionuclizii_$(librarie[begin:end-4]).png")
+end
+# Scatter cu error bars
+function Grafic_errorb(radionuclid, librarie)
     plt1 = scatter(
     radionuclid.A, 
     radionuclid.B, 
     yerr = radionuclid.σᴮ, 
     marker = :xcross,
-    markersize = 3, 
+    markersize = 2.5, 
+    markerstrokewidth = 0,
     xlabel = L"\mathrm{A}", 
     ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
     framestyle = :box,
     legend = :false,
     title = "Energia medie de legătură per nucleon, $(librarie[begin:end-4])",
     minorgrid = :true,
-    msc = :olivedrab,
-    mc = :cyan, 
+    msc = :purple,
+    mc = :olivedrab, 
     size = (1000, 1000)
     )
     annotate!(150, 6.5, L"\sigma_{B(A,Z)} = \frac{1}{A} \sqrt{Z^2 \sigma_{D_p}^2 + (A-Z)^2 \sigma_{D_n}^2 + \sigma_{D(A,Z)}^2}")
+    display(plt1)
     savefig(plt1, "Grafice\\B_toti_radionuclizii_errb_$(librarie[begin:end-4]).png")
-
-    return radionuclid # Obiectul returnat de functie este un vector de vectori 
+end
+# Scatter comparativ intre 2 biblioteci de date
+function Grafic_comparativ_lib(radionuclid_1, radionuclid_2, librarie_1, librarie_2)
+    plt1 = scatter(
+    radionuclid_1.A, 
+    radionuclid_1.B,  
+    marker = :xcross,
+    markersize = 3,
+    markerstrokewidth = 0, 
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z)\$ [MeV]"), 
+    framestyle = :box,
+    legend = :false,
+    title = "Energia medie de legătură per nucleon, $(librarie_1[begin:end-4])",
+    minorgrid = :true,
+    mc = :red, 
+    size = (900, 900)
+    )
+    plt2 = scatter(
+    radionuclid_2.A, 
+    radionuclid_2.B, 
+    marker = :xcross,
+    markersize = 3,
+    markerstrokewidth = 0, 
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z)\$ [MeV]"), 
+    framestyle = :box,
+    legend = :false,
+    title = "Energia medie de legătură per nucleon, $(librarie_2[begin:end-4])",
+    minorgrid = :true,
+    mc = :green, 
+    size = (900, 900)
+    )
+    plt = plot(plt1, plt2, layout = (2, 1))
+    display(plt)
+    savefig(plt, "Grafice\\B_comparat_$(librarie_1[begin:end-4])_$(librarie_2[begin:end-4]).png")
+end
+# Scatter in functie de paritatea A si Z
+function Grafice_paritati_combinate(radionuclid, librarie)
+    plt = scatter(
+    radionuclid.A[(iseven.(radionuclid.A) .== 1) .& (iseven.(radionuclid.Z) .== 1)], 
+    radionuclid.B[(iseven.(radionuclid.A) .== 1) .& (iseven.(radionuclid.Z) .== 1)],  
+    marker = :xcross,
+    markerstrokewidth = 0,
+    markersize = 3, 
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z)\$  [MeV]"), 
+    framestyle = :box,
+    label = "par-par",
+    title = "Energia medie de legătură per nucleon, $(librarie[begin:end-4])",
+    minorgrid = :true,
+    mc = :red, 
+    size = (1000, 1000)
+    )
+    plt = scatter!(
+    radionuclid.A[(iseven.(radionuclid.A) .== 1) .& (iseven.(radionuclid.Z) .== 0)], 
+    radionuclid.B[(iseven.(radionuclid.A) .== 1) .& (iseven.(radionuclid.Z) .== 0)],  
+    marker = :utriangle,
+    markersize = 3, 
+    markerstrokewidth = 0,
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z)\$  [MeV]"), 
+    framestyle = :box,
+    label = "impar-impar",
+    minorgrid = :true,
+    mc = :green, 
+    size = (1000, 1000)
+    )
+    plt = scatter!(
+    radionuclid.A[(iseven.(radionuclid.A) .== 0)], 
+    radionuclid.B[(iseven.(radionuclid.A) .== 0)],  
+    marker = :star5,
+    markersize = 3, 
+    markerstrokewidth = 0,
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z)\$  [MeV]"), 
+    framestyle = :box,
+    label = "A impar",
+    minorgrid = :true,
+    mc = :lightskyblue, 
+    size = (1000, 1000)
+    )     
+    display(plt)
+    savefig(plt, "Grafice\\B_paritati_combinat_$(librarie[begin:end-4]).png")    
+end
+# Scatter cu layout 3 linii, o coloana
+function Grafice_paritati_comparativ(radionuclid, librarie)
+    plt1 = scatter(
+    radionuclid.A[(iseven.(radionuclid.A) .== 1) .& (iseven.(radionuclid.Z) .== 1)], 
+    radionuclid.B[(iseven.(radionuclid.A) .== 1) .& (iseven.(radionuclid.Z) .== 1)],  
+    marker = :xcross,
+    markerstrokewidth = 0,
+    markersize = 3, 
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z)\$  [MeV]"), 
+    framestyle = :box,
+    label = "par-par",
+    title = "Energia medie de legătură per nucleon, $(librarie[begin:end-4])",
+    minorgrid = :true,
+    mc = :red, 
+    size = (900, 900)
+    )
+    plt2 = scatter(
+    radionuclid.A[(iseven.(radionuclid.A) .== 1) .& (iseven.(radionuclid.Z) .== 0)], 
+    radionuclid.B[(iseven.(radionuclid.A) .== 1) .& (iseven.(radionuclid.Z) .== 0)],  
+    marker = :utriangle,
+    markersize = 3, 
+    markerstrokewidth = 0,
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z)\$  [MeV]"), 
+    framestyle = :box,
+    label = "impar-impar",
+    minorgrid = :true,
+    mc = :green, 
+    size = (900, 900)
+    )
+    plt3 = scatter(
+    radionuclid.A[(iseven.(radionuclid.A) .== 0)], 
+    radionuclid.B[(iseven.(radionuclid.A) .== 0)],  
+    marker = :star5,
+    markersize = 3, 
+    markerstrokewidth = 0,
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$B(A,Z)\$  [MeV]"), 
+    framestyle = :box,
+    label = "A impar",
+    minorgrid = :true,
+    mc = :lightskyblue, 
+    size = (900, 900)
+    )   
+    plt = plot(plt1, plt2, plt3, layout = (3, 1))  
+    display(plt)
+    savefig(plt, "Grafice\\B_paritati_comparativ_$(librarie[begin:end-4]).png")    
 end
 
-Energie_medie_legatura("AUDI95.csv")
-Energie_medie_legatura("AUDI2021.csv")
+# Apelarea functiilor definite
+audi95 = "AUDI95.csv"
+audi21 = "AUDI2021.csv"
+radionuclid_95 = Energie_medie_legatura(audi95)
+radionuclid_21 = Energie_medie_legatura(audi2021)
 
-radionuclid = Energie_medie_legatura("AUDI95.csv")
-librarie = "AUDI95.csv"
-
-    plt1 = scatter(
-    radionuclid.A, 
-    radionuclid.B,  
-    marker = :xcross,
-    markersize = 3, 
-    xlabel = L"\mathrm{A}", 
-    ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
-    framestyle = :box,
-    legend = :false,
-    title = "Energia medie de legătură per nucleon",
-    minorgrid = :true,
-    mc = :blue, 
-    size = (900, 900)
-    )
-
-    plt2 = scatter(
-    radionuclid.A, 
-    radionuclid.B, 
-    yerr = radionuclid.σᴮ, 
-    marker = :xcross,
-    markersize = 3, 
-    xlabel = L"\mathrm{A}", 
-    ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
-    framestyle = :box,
-    legend = :false,
-    title = :false,
-    minorgrid = :true,
-    msc = :olivedrab,
-    mc = :cyan, 
-    size = (900, 900)
-    )
-
-    plt3 = scatter(
-    radionuclid.A, 
-    radionuclid.B, 
-    yerr = radionuclid.σᴮ, 
-    marker = :xcross,
-    markersize = 3, 
-    xlabel = L"\mathrm{A}", 
-    ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
-    framestyle = :box,
-    legend = :false,
-    title = :false,
-    minorgrid = :true,
-    msc = :olivedrab,
-    mc = :cyan, 
-    size = (900, 900)
-    )
-
-    plt4 = scatter(
-    radionuclid.A, 
-    radionuclid.B, 
-    yerr = radionuclid.σᴮ, 
-    marker = :xcross,
-    markersize = 3, 
-    xlabel = L"\mathrm{A}", 
-    ylabel = latexstring("\$B(A,Z) = \\frac{W(A,Z)}{A}\$  [MeV]"), 
-    framestyle = :box,
-    legend = :false,
-    title = :false,
-    minorgrid = :true,
-    msc = :olivedrab,
-    mc = :cyan, 
-    size = (900, 900)
-    )    
-
-    plt = plot(plt1, plt2, plt3, plt4, layout = (2, 2))
-    savefig(plt, "Grafice\\B_layout_4.png")
+Grafic_simplu(radionuclid_95, audi95)
+Grafic_simplu(radionuclid_21, audi21)
+Grafic_errorb(radionuclid_95, audi95)
+Grafic_errorb(radionuclid_21, audi21)
+Grafic_comparativ_lib(radionuclid_95, radionuclid_21, audi95, audi2021)
+Grafice_paritati_combinate(radionuclid_95, audi95)
+Grafice_paritati_combinate(radionuclid_21, audi21)
+Grafice_paritati_comparativ(radionuclid_95, audi95)
+Grafice_paritati_comparativ(radionuclid_21, audi21)
