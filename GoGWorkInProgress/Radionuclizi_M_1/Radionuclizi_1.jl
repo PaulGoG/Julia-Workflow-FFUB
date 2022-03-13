9f4aa8a1be8888e59dbe8b5b43439e8bebc38147
Paul-Adrian Gogîță <gogitapaul@yahoo.ro>
2022-03-13 15:29:58 +0200

    Tema_1

--- GoGWorkInProgress/Radionuclizi_M_1/Radionuclizi_1.jl
+++ GoGWorkInProgress/Radionuclizi_M_1/Radionuclizi_1.jl
@@ -14,9 +14,15 @@ struct Radionuclid
     σᴮ
 end
 
+struct Diferente
+    Z
+    A
+    εᴰ
+    σᴰ
+end
+
 function Energie_medie_legatura(librarie)
     radionuclid = Radionuclid(Int[], Int[], Float64[], Float64[])
-
     # Citim fisierul de tip CSV
     # Z|A|Simbol|D(KeV)|σᴰ(KeV) -> forma tabelului
     df = CSV.File(librarie; delim=' ', ignorerepeated=true, header=["Z", "A", "Sym", "D", "σ"]) |> DataFrame
@@ -57,8 +63,44 @@ function Energie_medie_legatura(librarie)
 
     return radionuclid # Obiectul returnat de functie este un vector de vectori de tipul structului
 end
-# Reprezentari grafice
 
+function Diferente_relative(librarie1, librarie2, trunchiere_sup, trunchiere_inf)
+    diferente = Diferente(Int[], Int[], Float64[], Float64[])
+    # Citim fisierele
+    # Z|A|Simbol|D(KeV)|σᴰ(KeV) -> forma tabelului
+    df1 = CSV.File(librarie1; delim=' ', ignorerepeated=true, header=["Z", "A", "Sym", "D", "σ"]) |> DataFrame
+    df2 = CSV.File(librarie2; delim=' ', ignorerepeated=true, header=["Z", "A", "Sym", "D", "σ"]) |> DataFrame
+
+    # Indexam parcurgerea librariilor de date dupa prima, si impunem existenta campului de date in cea de-a doua
+    for i in 1:maximum(df1.A)
+            Z_min = minimum(df1.Z[df1.A .== i])
+            Z_max = maximum(df1.Z[df1.A .== i])
+            for j in Z_min:Z_max 
+                # Verifica daca elementul (A,Z) exista in a doua librarie de date
+                if isassigned(df2.Z[df2.A .== i], j - Z_min + 1) 
+                    D1 = abs(df1.D[(df1.A .== i) .& (df1.Z .== j)][1])
+                    σᴰ1 = df1.σ[(df1.A .== i) .& (df1.Z .== j)][1]
+                    D2 = abs(df2.D[(df2.A .== i) .& (df2.Z .== j)][1])
+                    σᴰ2 = df2.σ[(df2.A .== i) .& (df2.Z .== j)][1]
+
+                    if D1 !=0 && D2 !=0                         
+                        εᴰ = 100 * abs(D1-D2)/D1 # Diferenta relativa data in procente
+                        if εᴰ <= trunchiere_sup && εᴰ >= trunchiere_inf # Ignoram valorile peste un procent dat pentru buna scalare a graficului
+                            σᴰ = 100 * sqrt((1 + D2/D1^2)^2 * σᴰ1^2 + (1 - 1/D1)^2 * σᴰ2^2) # Incertitudinea
+                                
+                            push!(diferente.εᴰ, εᴰ)
+                            push!(diferente.σᴰ, σᴰ)
+                            push!(diferente.A, i)
+                            push!(diferente.Z, j)
+                        end
+                    end
+                end
+            end
+    end 
+    return diferente
+end
+
+# Reprezentari grafice
 # Scatter simplu
 function Grafic_simplu(radionuclid, librarie)
     plt = scatter(
@@ -234,12 +276,38 @@ function Grafice_paritati_comparativ(radionuclid, librarie)
     display(plt)
     savefig(plt, "Grafice\\B_paritati_comparativ_$(librarie[begin:end-4]).png")    
 end
+# Scatter diferente relative intre masele a doua librarii de date
+function Grafic_diferente(diferente, librarie1, librarie2, trunchiere_sup, trunchiere_inf)
+    plt1 = scatter(
+    diferente.A, 
+    diferente.εᴰ, 
+    #yerr = diferente.σᴰ, 
+    marker = :xcross,
+    markersize = 3, 
+    xlabel = L"\mathrm{A}", 
+    ylabel = latexstring("\$\\varepsilon_{\\textrm{D}} = \\frac{|D_{\\textrm{$(librarie1[begin:end-4])}} - D_{\\textrm{$(librarie2[begin:end-4])}}|}{D_{\\textrm{$(librarie1[begin:end-4])}}}\$ exprimat în procente"), 
+    framestyle = :box,
+    legend = :false,
+    title = "Diferentele relative ale defectelor de masă între librăriile $(librarie1[begin:end-4]) și $(librarie2[begin:end-4])",
+    minorgrid = :true,
+    msc = :red,
+    mc = :blue, 
+    size = (1000, 1000)
+    )
+    annotate!(diferente.A[Int(ceil(length(diferente.A)/2))], Int(ceil(trunchiere_sup*0.8)), latexstring("\$\\varepsilon_{\\textrm{D}} \\in [$(trunchiere_inf)\\%, $(trunchiere_sup)\\%]\$"))
+    #annotate!(diferente.A[Int(ceil(length(diferente.A)/2))], Int(ceil(trunchiere_sup*0.6)), latexstring("\$\\sigma_{\\varepsilon_{\\textrm{D}}} = 100 \\sqrt{(1 + \\frac{D_{\\textrm{$(librarie2[begin:end-4])}}}{D^2_{\\textrm{$(librarie1[begin:end-4])}}})^2 \\sigma^2_{D_{\\textrm{$(librarie1[begin:end-4])}}} + (1 - \\frac{1}{D_{\\textrm{$(librarie1[begin:end-4])}}})^2 \\sigma^2_{D_{\\textrm{$(librarie2[begin:end-4])}}}}\$"))
+    display(plt1)
+    savefig(plt1, "Grafice\\Diferente_relative_defecte_mase_errb_$(librarie1[begin:end-4])_$(librarie2[begin:end-4])_trunch_$(trunchiere_inf)_$(trunchiere_sup).png")
+end
 
 # Apelarea functiilor definite
 audi95 = "AUDI95.csv"
 audi21 = "AUDI2021.csv"
+trunchiere_sup = 10
+trunchiere_inf = 0.05
 radionuclid_95 = Energie_medie_legatura(audi95)
 radionuclid_21 = Energie_medie_legatura(audi21)
+diferente = Diferente_relative(audi95, audi21, trunchiere_sup, trunchiere_inf)
 
 Grafic_simplu(radionuclid_95, audi95)
 Grafic_simplu(radionuclid_21, audi21)
@@ -250,3 +318,5 @@ Grafice_paritati_combinate(radionuclid_95, audi95)
 Grafice_paritati_combinate(radionuclid_21, audi21)
 Grafice_paritati_comparativ(radionuclid_95, audi95)
 Grafice_paritati_comparativ(radionuclid_21, audi21)
+Grafic_diferente(diferente, audi95, audi21, trunchiere_sup, trunchiere_inf)
+Grafic_diferente(diferente, audi21, audi95, trunchiere_sup, trunchiere_inf)
