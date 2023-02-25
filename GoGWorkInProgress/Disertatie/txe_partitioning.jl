fe72c7c8ce8ad906a5da3a636df028b8154fcbaf
Paul-Adrian Gogîță <gogitapaul@yahoo.ro>
2023-02-25 12:43:33 +0200

    Update txe_partitioning.jl

--- GoGWorkInProgress/Disertatie/txe_partitioning.jl
+++ GoGWorkInProgress/Disertatie/txe_partitioning.jl
@@ -9,40 +9,42 @@ deformation energy at scission and deformation energy at total acceleration of t
 =#
 function TXE_partitioning_MSCZ(A_0, Z_0, A_H_min, A_H_max, Eₙ, fragmdomain, dΔE_def, tkerange, density_parameter_type, density_parameter_datafile, dm)
     E_excit = Distribution(Int[], Int[], Float64[], Int[], Float64[], Float64[])
-    for A_H in A_H_min:A_H_max
-        A_L = A_0 - A_H
-        for i in eachindex(fragmdomain.Z[fragmdomain.A .== A_H])
-            Z_H = fragmdomain.Z[fragmdomain.A .== A_H][i]
-            Z_L = Z_0 - Z_H
-            if isassigned(dΔE_def.Value[(dΔE_def.A .== A_H) .& (dΔE_def.Z .== Z_H)], 1) && isassigned(dΔE_def.Value[(dΔE_def.A .== A_L) .& (dΔE_def.Z .== Z_L)], 1)
-                ΔE_def_H = dΔE_def.Value[(dΔE_def.A .== A_H) .& (dΔE_def.Z .== Z_H)][1]
-                ΔE_def_L = dΔE_def.Value[(dΔE_def.A .== A_L) .& (dΔE_def.Z .== Z_L)][1]
-                Q = Q_value_released(A_0, Z_0, A_H, Z_H, dm)
-                Sₙ = Separation_energy(1, 0, A_0, Z_0, dm)
-                Sₙ_L = Separation_energy(1, 0, A_L, Z_L, dm)[1]
-                Sₙ_H = Separation_energy(1, 0, A_H, Z_H, dm)[1]
-                if !isnan(Q[1]) && !isnan(Sₙ[1])
-                    for j in eachindex(tkerange)
-                        TXE = Total_excitation_energy(Q[1], Q[2], tkerange[j], 0.0, Sₙ[1], Sₙ[2], Eₙ)[1]
-                        if TXE > 0 
-                            a = density_parameter(density_parameter_type, A_0, Z_0, A_H, Z_H, density_parameter_datafile)
-                            if !isnan(a[1])
-                                if a[1] > 0 && a[2] > 0
-                                    r = a[1]/a[2]
-                                    E_scission = TXE - (ΔE_def_L + ΔE_def_H)
-                                    if E_scission > 0
-                                        E_excit_L = E_scission/(1 + r) + ΔE_def_L
-                                        E_excit_H = r * E_scission/(1 + r) + ΔE_def_H
-                                        if E_excit_L > Sₙ_L || E_excit_H > Sₙ_H
-                                            push!(E_excit.A, A_H)
-                                            push!(E_excit.Z, Z_H)
-                                            push!(E_excit.TKE, tkerange[j])
-                                            push!(E_excit.Value, E_excit_H)
-                                            if A_L != A_H
-                                                push!(E_excit.A, A_L)
-                                                push!(E_excit.Z, Z_L)
+    Sₙ = Separation_energy(1, 0, A_0, Z_0, dm)
+    if !isnan(Sₙ[1])
+        for A_H in A_H_min:A_H_max
+            A_L = A_0 - A_H
+            for i in eachindex(fragmdomain.Z[fragmdomain.A .== A_H])
+                Z_H = fragmdomain.Z[fragmdomain.A .== A_H][i]
+                Z_L = Z_0 - Z_H
+                if isassigned(dΔE_def.Value[(dΔE_def.A .== A_H) .& (dΔE_def.Z .== Z_H)], 1) && isassigned(dΔE_def.Value[(dΔE_def.A .== A_L) .& (dΔE_def.Z .== Z_L)], 1)
+                    ΔE_def_H = dΔE_def.Value[(dΔE_def.A .== A_H) .& (dΔE_def.Z .== Z_H)][1]
+                    ΔE_def_L = dΔE_def.Value[(dΔE_def.A .== A_L) .& (dΔE_def.Z .== Z_L)][1]
+                    Q = Q_value_released(A_0, Z_0, A_H, Z_H, dm)
+                    Sₙ_L = Separation_energy(1, 0, A_L, Z_L, dm)[1]
+                    Sₙ_H = Separation_energy(1, 0, A_H, Z_H, dm)[1]
+                    if !isnan(Q[1])
+                        for j in eachindex(tkerange)
+                            TXE = Total_excitation_energy(Q[1], Q[2], tkerange[j], 0.0, Sₙ[1], Sₙ[2], Eₙ)[1]
+                            if TXE > 0 
+                                a = density_parameter(density_parameter_type, A_0, Z_0, A_H, Z_H, density_parameter_datafile)
+                                if !isnan(a[1])
+                                    if a[1] > 0 && a[2] > 0
+                                        r = a[1]/a[2]
+                                        E_scission = TXE - (ΔE_def_L + ΔE_def_H)
+                                        if E_scission > 0
+                                            E_excit_L = E_scission/(1 + r) + ΔE_def_L
+                                            E_excit_H = r * E_scission/(1 + r) + ΔE_def_H
+                                            if E_excit_L > Sₙ_L || E_excit_H > Sₙ_H
+                                                push!(E_excit.A, A_H)
+                                                push!(E_excit.Z, Z_H)
                                                 push!(E_excit.TKE, tkerange[j])
-                                                push!(E_excit.Value, E_excit_L)
+                                                push!(E_excit.Value, E_excit_H)
+                                                if A_L != A_H
+                                                    push!(E_excit.A, A_L)
+                                                    push!(E_excit.Z, Z_L)
+                                                    push!(E_excit.TKE, tkerange[j])
+                                                    push!(E_excit.Value, E_excit_L)
+                                                end
                                             end
                                         end
                                     end
@@ -58,33 +60,35 @@ function TXE_partitioning_MSCZ(A_0, Z_0, A_H_min, A_H_max, Eₙ, fragmdomain, d�
 end
 function TXE_partitioning_PARAM(A_0, Z_0, A_H_min, A_H_max, Eₙ, fragmdomain, dRatio, tkerange, dm)
     E_excit = Distribution(Int[], Int[], Float64[], Int[], Float64[], Float64[])
-    for A_H in A_H_min:A_H_max
-        A_L = A_0 - A_H
-        for i in eachindex(fragmdomain.Z[fragmdomain.A .== A_H])
-            Z_H = fragmdomain.Z[fragmdomain.A .== A_H][i]
-            Z_L = Z_0 - Z_H
-            if isassigned(dRatio.Value[(dRatio.A .== A_H) .& (dRatio.Z .== Z_H)], 1)
-                Ratio = dRatio.Value[(dRatio.A .== A_H) .& (dRatio.Z .== Z_H)][1]
-                Q = Q_value_released(A_0, Z_0, A_H, Z_H, dm)
-                Sₙ = Separation_energy(1, 0, A_0, Z_0, dm)
-                Sₙ_L = Separation_energy(1, 0, A_L, Z_L, dm)[1]
-                Sₙ_H = Separation_energy(1, 0, A_H, Z_H, dm)[1]
-                if !isnan(Q[1]) && !isnan(Sₙ[1])
-                    for j in eachindex(tkerange)
-                        TXE = Total_excitation_energy(Q[1], Q[2], tkerange[j], 0.0, Sₙ[1], Sₙ[2], Eₙ)[1]
-                        if TXE > 0 
-                            E_excit_H = TXE * Ratio
-                            E_excit_L = TXE - E_excit_H
-                            if E_excit_L > Sₙ_L || E_excit_H > Sₙ_H
-                                push!(E_excit.A, A_H)
-                                push!(E_excit.Z, Z_H)
-                                push!(E_excit.TKE, tkerange[j])
-                                push!(E_excit.Value, E_excit_H)
-                                if A_L != A_H
-                                    push!(E_excit.A, A_L)
-                                    push!(E_excit.Z, Z_L)
+    Sₙ = Separation_energy(1, 0, A_0, Z_0, dm)
+    if !isnan(Sₙ[1])
+        for A_H in A_H_min:A_H_max
+            A_L = A_0 - A_H
+            for i in eachindex(fragmdomain.Z[fragmdomain.A .== A_H])
+                Z_H = fragmdomain.Z[fragmdomain.A .== A_H][i]
+                Z_L = Z_0 - Z_H
+                if isassigned(dRatio.Value[(dRatio.A .== A_H) .& (dRatio.Z .== Z_H)], 1)
+                    Ratio = dRatio.Value[(dRatio.A .== A_H) .& (dRatio.Z .== Z_H)][1]
+                    Q = Q_value_released(A_0, Z_0, A_H, Z_H, dm)
+                    Sₙ_L = Separation_energy(1, 0, A_L, Z_L, dm)[1]
+                    Sₙ_H = Separation_energy(1, 0, A_H, Z_H, dm)[1]
+                    if !isnan(Q[1])
+                        for j in eachindex(tkerange)
+                            TXE = Total_excitation_energy(Q[1], Q[2], tkerange[j], 0.0, Sₙ[1], Sₙ[2], Eₙ)[1]
+                            if TXE > 0 
+                                E_excit_H = TXE * Ratio
+                                E_excit_L = TXE - E_excit_H
+                                if E_excit_L > Sₙ_L || E_excit_H > Sₙ_H
+                                    push!(E_excit.A, A_H)
+                                    push!(E_excit.Z, Z_H)
                                     push!(E_excit.TKE, tkerange[j])
-                                    push!(E_excit.Value, E_excit_L)
+                                    push!(E_excit.Value, E_excit_H)
+                                    if A_L != A_H
+                                        push!(E_excit.A, A_L)
+                                        push!(E_excit.Z, Z_L)
+                                        push!(E_excit.TKE, tkerange[j])
+                                        push!(E_excit.Value, E_excit_L)
+                                    end
                                 end
                             end
                         end
