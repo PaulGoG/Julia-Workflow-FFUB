24ae01430c50e1f5c88eacf7de54bc05be98fc08
PaulGoG <gogitapaul@yahoo.ro>
2023-03-09 12:09:28 +0200

    implemented basic surface plots

--- GoGWorkInProgress/Disertatie/plots.jl
+++ GoGWorkInProgress/Disertatie/plots.jl
@@ -4,24 +4,62 @@ if !isdir("plots/")
 end
 
 using Plots
-plotlyjs(size = (600, 800))
+plotlyjs(size = plots_resolution)
 #####
-function Grid_builder(x::AbstractVector, y::AbstractVector, z::AbstractVector)
-    
-end
-
-#Testing for plots
-Grid = zeros(length(unique(y_Ap_Z.A)), length(unique(y_Ap_Z.Z)))
-i1 = 0
-for A in unique(y_Ap_Z.A)
-    i1 += 1
-    i2 = 1
-    for Z in unique(y_Ap_Z.Z)
-        if isassigned(y_Ap_Z.Value[(y_Ap_Z.A .== A) .& (y_Ap_Z.Z .== Z)], 1)
-            Grid[i1, i2] = y_Ap_Z.Value[(y_Ap_Z.A .== A) .& (y_Ap_Z.Z .== Z)][1]  
+function Grid_builder(q_x_y::DataFrame)
+    x = sort(unique(q_x_y.x))
+    y = sort(unique(q_x_y.y))
+    grid = zeros(length(x), length(y))
+    for index_x in eachindex(x)
+        for index_y in eachindex(y)
+            if isassigned(q_x_y.z[(q_x_y.x .== x[index_x]) .& (q_x_y.y .== y[index_y])], 1)
+                grid[index_x, index_y] = q_x_y.z[(q_x_y.x .== x[index_x]) .& (q_x_y.y .== y[index_y])][1] 
+            end
         end
-        i2 += 1
     end
+    return grid
+end
+function Plot_surface(q_x_y::DataFrame, tick_size_xaxis::Int, tick_roundness_xaxis, tick_size_yaxis::Int, tick_roundness_yaxis, camera::Tuple)
+    grid_q_x_y = Grid_builder(q_x_y)
+
+    x = sort(unique(q_x_y.x))
+    first_index_x = findfirst(x -> round(x/tick_roundness_xaxis) == x/tick_roundness_xaxis, x)
+    last_index_x = findlast(x -> round(x/tick_roundness_xaxis) == x/tick_roundness_xaxis, x)
+    index_x_range = first_index_x:tick_size_xaxis:last_index_x
+
+    y = sort(unique(q_x_y.y))
+    first_index_y = findfirst(x -> round(x/tick_roundness_yaxis) == x/tick_roundness_yaxis, y)
+    last_index_y = findlast(x -> round(x/tick_roundness_yaxis) == x/tick_roundness_yaxis, y)
+    index_y_range = first_index_y:tick_size_yaxis:last_index_y
+
+    plt = surface(
+        grid_q_x_y', 
+        xticks = (collect(index_x_range), string.(Int.(round.(x[index_x_range])))), 
+        yticks = (collect(index_y_range), string.(Int.(round.(y[index_y_range]))))
+    )
+    return plt
 end
 
-surface(Grid', zscale=:log10, zlims=(1e-1, maximum(Grid)))
+function Display_plot(plt, filename)
+    display(plt)
+    #savefig("plots/$filename.png")
+end
+#####
+
+plot_surface_ν_A_TKE = Plot_surface(
+    DataFrame(x = ν_A_TKE.A, y = ν_A_TKE.TKE, z = ν_A_TKE.Value),
+    10,
+    10,
+    Int(round(10/TKE_step)),
+    10
+    )
+display(plot_surface_ν_A_TKE)
+
+plot_surface_y_Ap_Z = Plot_surface(
+    DataFrame(x = y_Ap_Z.A, y = y_Ap_Z.Z, z = y_Ap_Z.Value),
+    10,
+    10,
+    2,
+    10
+    )
+display(plot_surface_y_Ap_Z)
