277930465ceedc6dc0b9efa0cfb4cce1ec06ef4d
Paul-Adrian Gogîță <gogitapaul@yahoo.ro>
2023-03-30 19:37:15 +0300

    done for today

--- GoGWorkInProgress/Disertatie/input_52CF.jl
+++ GoGWorkInProgress/Disertatie/input_52CF.jl
@@ -91,7 +91,7 @@ txe_partitioning_header = ["A", "Z", "Value"]
 txe_partitioning_delimiter = ' '
 txe_partitioning_firstdataline = 2
 
-txe_partitioning_segmentpoints = [(126, 1.0), (130, 1.734), (134, 1.28), (138, 1.28), (150, 0.9), (174, 0.59)]
+txe_partitioning_segmentpoints = [(126, 1.0), (130, 1.734), (134, 1.28), (138, 1.28), (150, 0.9), (167, 0.68), (174, 0.59)]
 
 #Writing out main DSE output file containing detailed sequence data YES or NO selector
 write_primary_output = "YES"
